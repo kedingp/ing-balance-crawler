3702446d99256e5133e94b6b521a3bb19a370480
Philipp Keding <Philipp.Keding@cslbehring.com>
2024-01-16 11:25:26 +0100

    Update check_dropbox.py

--- check_dropbox.py
+++ check_dropbox.py
@@ -1,19 +1,25 @@
 import requests
 import json
-
 import os
 
 
 dropbox_api_key = os.environ.get('DROPBOX_API_KEY')
 
-url = "https://api.dropboxapi.com/2/file_requests/count"
+
+
+url = "https://api.dropboxapi.com/2/files/list_folder"
 
 headers = {
     "Authorization": f"Bearer {dropbox_api_key}",
     "Content-Type": "application/json"
 }
 
-data = None
+data = {
+    "path": ""
+}
 
 r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r.status_code)
+
+print("Response Content:")
+print(r.text)

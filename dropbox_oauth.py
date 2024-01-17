28bc0ff76a5240127d4c0fc05632e39dfec73ae2
Philipp Keding <Philipp.Keding@cslbehring.com>
2024-01-17 10:46:48 +0100

    Update dropbox_oauth.py

--- dropbox_oauth.py
+++ dropbox_oauth.py
@@ -2,22 +2,37 @@
 
 import requests
 import os
+def get_access_token():
+    url = 'https://api.dropboxapi.com/oauth2/token'
+    app_key = os.environ.get('DROPBOX_KEY')
+    app_secret = os.environ.get('DROPBOX_SECRET')
+    refresh_token = os.environ.get("DROPBOX_REFRESH_TOKEN")
 
-url = 'https://api.dropboxapi.com/oauth2/token'
-app_key = os.environ.get('DROPBOX_KEY')
-app_secret = os.environ.get('DROPBOX_SECRET')
-refresh_token = os.environ.get("DROPBOX_REFRESH_TOKEN")
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
+    data = {
+        'refresh_token': refresh_token,
+        'grant_type': 'refresh_token'
+    }
 
-data = {
-    'refresh_token': refresh_token,
-    'grant_type': 'refresh_token'
-}
+    auth = (app_key, app_secret)
+    response = requests.post(url, headers=headers, data=data, auth=auth)
 
-auth = (app_key, app_secret)
-response = requests.post(url, headers=headers, data=data, auth=auth)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        json_response = response.json()
 
-print(response.text)
+        # Extract the access_token
+        access_token = json_response.get('access_token')
+
+        # Print or use the access_token
+        return access_token
+    else:
+        # Print the error message if the request was not successful
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+print(f"Access token: {get_access_token()}")

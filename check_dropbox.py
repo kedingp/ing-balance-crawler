edb59bd36ba547b9997c83bbccfb28251dae2b2f
Philipp Keding <Philipp.Keding@cslbehring.com>
2024-01-17 11:31:00 +0100

    Update check_dropbox.py

--- check_dropbox.py
+++ check_dropbox.py
@@ -1,26 +1,39 @@
 import requests
 import json
-import os
 import dropbox_oauth
 
 
-dropbox_api_key =  dropbox_oauth.get_access_token()
 
 
 
-url = "https://api.dropboxapi.com/2/files/list_folder"
+def all_files_in_folder(access_token):
+    url = "https://api.dropboxapi.com/2/files/list_folder"
 
-headers = {
-    "Authorization": f"Bearer {dropbox_api_key}",
-    "Content-Type": "application/json"
-}
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
 
-data = {
-    "path": ""
-}
+    data = {
+        "path": ""
+    }
 
-r = requests.post(url, headers=headers, data=json.dumps(data))
-print(r.status_code)
+    response = requests.post(url, headers=headers, data=json.dumps(data))
 
-print("Response Content:")
-print(r.text)
+        # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        json_response = response.json()
+
+        # Extract file names from the list of entries
+        file_names = [entry.get('name') for entry in json_response.get('entries', []) if entry.get('.tag') == 'file']
+        return file_names
+
+    else:
+        # Print the error message if the request was not successful
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+if __name__ == "__main__":
+    dropbox_api_key =  dropbox_oauth.get_access_token()
+    all_files_in_folder(dropbox_api_key)
